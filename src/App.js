398d3f30eacbeccaf27205133f834c7cedb99e70
kawan idrees <kawanedres211@gmail.com>
2022-02-05 01:32:43 +0300

    git some chages were needed to be done

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ function App() {
         <div className={`${isDark?"mainContainerDark":"mainContainer"}`}>
           {/* Banner  */}
           <div className="banner">
-            <BannerName name={"Kawan"} discount={"20"} more={"#"} />
+            <BannerName name={"Kawan"} />
             <img
               src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdelivery.png?alt=media&token=69b9823d-96df-452a-bd4e-14d27a4cc337"
               alt=""
--- src/Components/BannerName.js
+++ src/Components/BannerName.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-function BannerName({ name, discount, more }) {
-  const currency = "$";
+function BannerName({ name }) {
+  
   return (
     <div className="bannerContent">
       <h3>Hello {name},</h3>
       <p>
-        Get free discount for every <span>{`${currency}${discount}`}</span>{" "}
-        purchase
+      Enjoy your meal with our foods , we will serve you as far as we can 
       </p>
       <a href="https://www.linkedin.com/in/kawan-edres-babb66216/">About Me</a>
     </div>
